3438b010b1cf70267fca612432a2a999f0706319
ChelesteWang <1581446178@qq.com>
2022-04-15 10:40:58 +0800

    配置 prettier/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,12 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  rules: {
-  },
-};
+  rules: {}
+}
