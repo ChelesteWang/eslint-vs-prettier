ff4a1d36a6a0b21b0f20d01e92ee879a8636c480
ChelesteWang <1581446178@qq.com>
2022-04-15 11:23:12 +0800

    更新 eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,16 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: ['airbnb-base', 'prettier'], // 继承 eslint-config-prettier 的 rules 关闭 ESLint 中的涉及到 prettier 样式的规则
+  plugins: ['prettier'], // 注册 eslint-plugin-prettier
+  rules: {
+    'prettier/prettier': 'error', // 打开该插件提供的规则，并且是作为eslint的一条规则运行
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off'
+  },
+  // extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
-  },
-  rules: {}
+  }
 }
